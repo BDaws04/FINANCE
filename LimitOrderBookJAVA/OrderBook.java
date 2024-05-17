063a5e54c9fa75ca874540dffef98eba605355de
BDaws04 <ben.dawson@kcl.ac.uk>
2024-05-17 20:02:03 +0100

    comparators for the buy and sell heaps

--- LimitOrderBookJAVA/OrderBook.java
+++ LimitOrderBookJAVA/OrderBook.java
@@ -2,37 +2,53 @@ package LimitOrderBookJAVA;
 import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.HashMap;
+import java.util.Comparator;
 
 public class OrderBook {
 
     PriorityQueue<Order> sellOrders;
     PriorityQueue<Order> buyOrders;
     HashMap<Double, Integer> priceVolume;
+    Comparator<Order> sellOrderComparator;
+    Comparator<Order> buyOrderComparator;
 
 
     public OrderBook(){
-        sellOrders = new PriorityQueue<>();
+
+        sellOrders = new PriorityQueue<>(sellOrderComparator);
         buyOrders = new PriorityQueue<>(Collections.reverseOrder());
         priceVolume = new HashMap<>();
+        sellOrderComparator = Comparator
+                .comparing(Order::getPrice)
+                .thenComparing(Order::getOrderTime);
+        buyOrderComparator = Comparator
+                .comparing(Order::getPrice)
+                .reversed()
+                .thenComparing(Order::getOrderTime);
     }
 
     public void placeSellOrder(Order order){
         if (buyOrders.isEmpty()){
-            double price = order.getPrice();
-            int volume = order.getVolume();
-            if (priceVolume.containsKey(price)){
-                int currentVolume = priceVolume.get(price);
-                priceVolume.put(price, currentVolume + volume);
-            } else{
-                priceVolume.put(price, volume);
-            }
             addOrder(order);
+            updatePriceVolume(order);
+        }
+    }
+
+    private void updatePriceVolume(Order order){
+        double price = order.getPrice();
+        int volume = order.getVolume();
+        if (priceVolume.containsKey(price)){
+            int currentVolume = priceVolume.get(price);
+            priceVolume.put(price, currentVolume + volume);
+        } else{
+            priceVolume.put(price, volume);
         }
     }
 
     public void placeBuyOrder(Order order){
         if (sellOrders.isEmpty()){
             addOrder(order);
+            return;
         }
     }
 
