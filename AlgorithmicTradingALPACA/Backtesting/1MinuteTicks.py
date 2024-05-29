ab38a7b6c8d0823bfec2bfe0af5d62c9bed3e4f9
BDaws04 <ben.dawson@kcl.ac.uk>
2024-05-29 18:55:38 +0100

    Added graph drawing to compare stratgey to buyandhold and to the SPY

--- AlgorithmicTradingALPACA/Backtesting/1MinuteTicks.py
+++ AlgorithmicTradingALPACA/Backtesting/1MinuteTicks.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt 
 import pandas as pd
+from pandas import DataFrame
 import pandas_ta as ta 
 from alpaca.data.historical import StockHistoricalDataClient
 from alpaca.data.requests import StockBarsRequest
@@ -11,6 +12,34 @@ from datetime import datetime, timedelta
 File for testing trading strategies, where the required ticks is one minute
 This is a general file, particular changes for testing a particular stock/strategy should not be saved to this master file
 """
+
+def plotGraph(spy: DataFrame, stock: DataFrame, balance: float, label: str):
+    
+    spyPrices = spy['close']
+    stockPrices = stock['close']
+
+    spyStart = spyPrices.iloc[0]
+    stockStart = stockPrices.iloc[0]
+
+    spyMultiplier = balance / spyStart
+    stockMultiplier = balance / stockStart
+
+    spyArray = spyPrices.values
+    stockArray = stockPrices.values
+    spyArray *= spyMultiplier
+    stockArray *= stockMultiplier
+
+    plt.figure(figsize=(10,6))
+    plt.plot(spyArray, label = 'SPY', color='red')
+    plt.plot(stockArray, label=label, color='blue')
+
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.title('Price Comparison: SPY vs BUYANDHOLD vs Strategy')
+    plt.legend()
+    plt.grid(True)
+
+    plt.show()
 
 def loadData(api: str, secret: str, stock: str):
 
@@ -24,6 +53,12 @@ def loadData(api: str, secret: str, stock: str):
         start=oneYearAgoDateF,
         timeframe=TimeFrame.Day
     )
+    dailyDataParametersSPY = StockBarsRequest(
+        symbol_or_symbols='SPY',
+        start=oneYearAgoDateF,
+        timeframe=TimeFrame.Day
+    )
+
     minuteDataParameters = StockBarsRequest(
         symbol_or_symbols=stock,
         start=oneYearAgoDateF,
@@ -31,20 +66,20 @@ def loadData(api: str, secret: str, stock: str):
     )
     dailyData = client.get_stock_bars(dailyDataParameters)
     minuteData = client.get_stock_bars(minuteDataParameters)
+    spyData = client.get_stock_bars(dailyDataParametersSPY)
 
     dailyDataDF = dailyData.df
     minuteDataDF = minuteData.df
+    spyDataDF = spyData.df
 
-    return dailyDataDF, minuteDataDF
+    return dailyDataDF, minuteDataDF, spyDataDF
 
 def test(stock: str):
     API_KEY, SECRET_KEY = loadKeys()
     starting_balance = 10000
-    dailyData, minuteData = loadData(API_KEY, SECRET_KEY, stock)
+    dailyData, minuteData, spy = loadData(API_KEY, SECRET_KEY, stock)
 
-    print(len(dailyData))
-    print(len(minuteData))
-
-
+    plotGraph(spy, dailyData, 10000, stock)
+    
 if __name__ == '__main__':
     test('AAPL')
